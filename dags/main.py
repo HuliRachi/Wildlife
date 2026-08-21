fd5eaaf123e964330c8e86aba19be119131e8d38
hulisani <rachyhuly17@gmail.com>
2026-08-21 16:30:55 +0200

    feact(docker): add docker containerization

---
+++ dags/main.py
@@ -0,0 +1,42 @@
+from airflow import DAG
+import pendulum
+from datetime import datetime, timedelta
+from airflow.operators.trigger_dagrun import TriggerDagRunOperator
+
+from api.animals_data import (
+    fetch_raw_animal_data,
+    extract_animal_data,
+    save_to_json,
+)
+
+# from datawarehouse.dwh import staging_table, core_table
+# from dataquality.soda import yt_elt_data_quality
+
+local_tz = pendulum.timezone("Africa/Johannesburg")
+
+default_args = {
+    "owner": "Rachi Huli",
+    "depends_on_past": False,
+    "email_on_failure": False,
+    "email_on_retry": False,
+    "email": "rachy@gmail.com",
+    "max_active_runs": 1,
+    "dagrun_timeout": timedelta(hours=1),
+    "start_date": datetime(2025, 1, 1, tzinfo=local_tz),
+}
+
+with DAG(
+    dag_id="produce_json",
+    default_args=default_args,
+    description="DAG to produce JSON file with raw data",
+    schedule="0 14 * * *",
+    catchup=False,
+) as dag_produce:
+
+    raw_data = fetch_raw_animal_data()
+    cleaned_data = extract_animal_data(raw_data)
+    save_to_json_task = save_to_json(cleaned_data)
+
+
+
+    raw_data >> cleaned_data >> save_to_json_task
